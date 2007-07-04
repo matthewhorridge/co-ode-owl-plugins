9510828216c91aa89303d3209436e34317a71570
Nick Drummond <nickdrummond@users.noreply.github.com>
2007-07-04 14:03:25 +0000

    - initial import 1.0.0.browser

---
+++ browser/src/org/coode/browser/protege/ProtegeNameMapperWrapper.java
@@ -0,0 +1,85 @@
+package org.coode.browser.protege;
+
+import org.coode.html.OWLNameMapper;
+import org.semanticweb.owl.model.*;
+import org.protege.editor.owl.model.OWLModelManager;
+
+import java.util.Set;
+import java.util.Collections;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/*
+ * Copyright (C) 2007, University of Manchester
+ *
+ * Modifications to the initial code base are copyright of their
+ * respective authors, or their employers as appropriate.  Authorship
+ * of the modifications may be determined from the ChangeLog placed at
+ * the end of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Author: Nick Drummond<br>
+ *
+ * http://www.cs.man.ac.uk/~drummond<br><br>
+ * <p/>
+ * The University Of Manchester<br>
+ * Bio Health Informatics Group<br>
+ * Date: Jun 29, 2007<br><br>
+ * <p/>
+ */
+public class ProtegeNameMapperWrapper implements OWLNameMapper {
+
+    private OWLModelManager mngr;
+
+
+    public ProtegeNameMapperWrapper(OWLModelManager mngr) {
+        this.mngr = mngr;
+    }
+
+    public Set<OWLClass> getOWLClasses(String name) {
+        return Collections.singleton(mngr.getOWLClass(name));
+    }
+
+    public Set<OWLObjectProperty> getOWLObjectProperties(String name) {
+        return Collections.singleton(mngr.getOWLObjectProperty(name));
+    }
+
+    public Set<OWLDataProperty> getOWLDataProperties(String name) {
+        return Collections.singleton(mngr.getOWLDataProperty(name));
+    }
+
+    public Set<OWLProperty> getOWLProperties(String name) {
+        OWLProperty prop = mngr.getOWLObjectProperty(name);
+        if (prop == null){
+            prop = mngr.getOWLDataProperty(name);
+        }
+        return Collections.singleton(prop);
+    }
+
+    public Set<OWLIndividual> getOWLIndividuals(String name) {
+        return Collections.singleton(mngr.getOWLIndividual(name));
+    }
+
+    public Set<String> getClassNames() {
+        throw new NotImplementedException();
+    }
+
+    public Set<String> getObjectPropertyNames() {
+        throw new NotImplementedException();
+    }
+}
