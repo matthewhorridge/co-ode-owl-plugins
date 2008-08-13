a1fde323ad57e29be4966bd209214130d9aede38
Nick Drummond <nickdrummond@users.noreply.github.com>
2008-08-13 11:13:20 +0000

    Renamed "Annotate view" to "Annotation template view"

---
+++ org.coode.annotate/src/org/coode/annotate/TemplateModel.java
@@ -0,0 +1,226 @@
+package org.coode.annotate;
+
+import org.apache.log4j.Logger;
+import org.coode.annotate.prefs.AnnotationTemplatePrefs;
+import org.protege.editor.owl.model.OWLModelManager;
+import org.semanticweb.owl.model.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+/*
+* Copyright (C) 2007, University of Manchester
+*
+* Modifications to the initial code base are copyright of their
+* respective authors, or their employers as appropriate.  Authorship
+* of the modifications may be determined from the ChangeLog placed at
+* the end of this file.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Author: Nick Drummond<br>
+ * http://www.cs.man.ac.uk/~drummond/<br><br>
+ * <p/>
+ * The University Of Manchester<br>
+ * Bio Health Informatics Group<br>
+ * Date: Aug 16, 2007<br><br>
+ */
+public class TemplateModel {
+
+    private static final Logger logger = Logger.getLogger(TemplateModel.class.getName());
+
+    public static final String TEXT = "text";
+    public static final String ENTITY = "entity";
+    public static final String MULTILINE = "multiline";
+
+    private final java.util.List<TemplateRow> cList = new ArrayList<TemplateRow>();
+
+    private final AnnotationComponentComparator comparator = new AnnotationComponentComparator();
+
+    private final Set<TemplateModelListener> listeners = new HashSet<TemplateModelListener>();
+
+    private final OWLModelManager mngr;
+
+    private OWLEntity currentEntity;
+
+    private OWLOntologyChangeListener ontChangeListener = new OWLOntologyChangeListener(){
+        public void ontologiesChanged(List<? extends OWLOntologyChange> list) throws OWLException {
+            handleOntologyChanges(list);
+        }
+    };
+
+
+    public TemplateModel(OWLModelManager mngr) throws IOException {
+        this.mngr = mngr;
+
+        mngr.addOntologyChangeListener(ontChangeListener);
+    }
+
+
+    public String getComponentType(URI uri){
+        Set<String> params = AnnotationTemplatePrefs.getInstance().getParams(uri);
+
+        // the order of these will determine precedence if multiple component types are specified
+        if (params.contains(ENTITY)){
+            return ENTITY;
+        }
+        else if (params.contains(MULTILINE)){
+            return MULTILINE;
+        }
+        else {// default to text (params.contains(TEXT)){
+            return TEXT;
+        }
+    }
+
+    public Set<OWLAnnotationAxiom> getAnnotations(OWLEntity entity) {
+        Set<OWLAnnotationAxiom> annotations = new HashSet<OWLAnnotationAxiom>();
+        for (OWLOntology ont : mngr.getActiveOntologies()){
+            annotations.addAll(entity.getAnnotationAxioms(ont));
+        }
+        return annotations;
+    }
+
+
+    public Set<OWLOntology> getOntologiesContainingAnnotation(TemplateRow templateRow) {
+        Set<OWLOntology> onts = new HashSet<OWLOntology>();
+        OWLAxiom ax = templateRow.getAxiom();
+        for (OWLOntology ont : mngr.getActiveOntologies()){
+            if (ont.containsAxiom(ax)){
+                onts.add(ont);
+            }
+        }
+        return onts;
+    }
+
+    public OWLModelManager getOWLModelManager() {
+        return mngr;
+    }
+
+    public OWLEntity getEntity() {
+        return currentEntity;
+    }
+
+    private void handleOntologyChanges(List<? extends OWLOntologyChange> changes) {
+        for (OWLOntologyChange change : changes){
+            if (change.isAxiomChange() &&
+                change.getAxiom() instanceof OWLEntityAnnotationAxiom &&
+                ((OWLEntityAnnotationAxiom)change.getAxiom()).getSubject().equals(currentEntity)){
+                refresh();
+                return;
+            }
+        }
+    }
+
+    private void refresh(){
+        setEntity(currentEntity);
+    }
+
+    public void setEntity(OWLEntity entity) {
+        currentEntity = entity;
+        cList.clear();
+
+        if (entity != null){
+            Set<OWLAnnotationAxiom> annots = getAnnotations(entity);
+            Set<URI> usedURIs = new HashSet<URI>();
+            for (OWLAnnotationAxiom annot : annots){
+                final URI annotationURI = annot.getAnnotation().getAnnotationURI();
+                if (AnnotationTemplatePrefs.getInstance().getURIList().contains(annotationURI)){
+                    usedURIs.add(annotationURI);
+                    cList.add(new TemplateRow(annot, this));
+                }
+            }
+
+            for (URI uri : AnnotationTemplatePrefs.getInstance().getURIList()){
+                if (!usedURIs.contains(uri)){
+                    cList.add(new TemplateRow(entity, uri, this));
+                }
+            }
+
+            Collections.sort(cList, comparator);
+        }
+
+        notifyStructureChanged();
+    }
+
+    public List<TemplateRow> getRows() {
+        return Collections.unmodifiableList(cList);
+    }
+
+    public void addRow(URI uri) {
+        cList.add(new TemplateRow(currentEntity, uri, this));
+        Collections.sort(cList, comparator);
+        notifyStructureChanged();
+    }
+
+    public void removeRow(TemplateRow c) {
+        c.setValue(null);
+        cList.remove(c);
+        notifyStructureChanged();
+    }
+
+    private void notifyStructureChanged() {
+        for (TemplateModelListener l : listeners){
+            l.modelStructureChanged();
+        }
+    }
+
+    public void addModelListener(TemplateModelListener l){
+        listeners.add(l);
+    }
+
+    public void removeModelListener(TemplateModelListener l){
+        listeners.remove(l);
+    }
+
+    /**
+     * Called by the rows when they make changes
+     * (so that the table can manage itself without having to refresh completely)
+     * This implementation disables the ontology change listeners temporarily
+     *
+     * @param changes the set of changes to apply
+     */
+    public void requestApplyChanges(List<OWLOntologyChange> changes) {
+        if (!changes.isEmpty()){
+            mngr.removeOntologyChangeListener(ontChangeListener);
+            mngr.applyChanges(changes);
+            mngr.addOntologyChangeListener(ontChangeListener);
+        }
+    }
+
+    public void dispose() {
+        mngr.removeOntologyChangeListener(ontChangeListener);
+        listeners.clear();
+    }
+
+    class AnnotationComponentComparator implements Comparator<TemplateRow> {
+
+        public int compare(TemplateRow c1, TemplateRow c2) {
+            URI uri1 = c1.getURI();
+            URI uri2 = c2.getURI();
+            for (URI uri : AnnotationTemplatePrefs.getInstance().getURIList()){
+                if (uri.equals(uri1)){
+                    return -1;
+                }
+                if (uri.equals(uri2)){
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ org.coode.annotate/src/org/coode/annotate/prefs/AnnotationTemplatePrefsPanel.java
@@ -0,0 +1,269 @@
+package org.coode.annotate.prefs;
+
+import org.protege.editor.core.ui.util.Icons;
+import org.protege.editor.core.ui.util.UIUtil;
+import org.protege.editor.owl.ui.OWLIcons;
+import org.protege.editor.owl.ui.frame.AnnotationURIList;
+import org.protege.editor.owl.ui.preferences.OWLPreferencesPanel;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+/*
+* Copyright (C) 2007, University of Manchester
+*
+* Modifications to the initial code base are copyright of their
+* respective authors, or their employers as appropriate.  Authorship
+* of the modifications may be determined from the ChangeLog placed at
+* the end of this file.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Author: drummond<br>
+ * http://www.cs.man.ac.uk/~drummond/<br><br>
+ * <p/>
+ * The University Of Manchester<br>
+ * Bio Health Informatics Group<br>
+ * Date: Apr 1, 2008<br><br>
+ */
+public class AnnotationTemplatePrefsPanel extends OWLPreferencesPanel {
+
+    private static final String TEMPLATE_EXT = "template";
+    
+    private JTable table;
+    private JToolBar toolbar;
+
+    private MyTableModel model;
+
+    private boolean dirty = false;
+
+    private Action addAction = new AbstractAction("Add Annotation", OWLIcons.getIcon("property.annotation.add.png")){
+        public void actionPerformed(ActionEvent actionEvent) {
+            handleAddAnnotation();
+        }
+    };
+
+    private Action removeAction = new AbstractAction("Remove Annotation", OWLIcons.getIcon("property.annotation.remove.png")){
+        public void actionPerformed(ActionEvent actionEvent) {
+            handleRemoveAnnotation();
+        }
+    };
+
+    private Action upAction = new AbstractAction("Move Up", Icons.getIcon("object.move_up.gif")){
+        public void actionPerformed(ActionEvent actionEvent) {
+            handleMoveUp();
+        }
+    };
+
+    private Action downAction = new AbstractAction("Move Down", Icons.getIcon("object.move_down.gif")){
+        public void actionPerformed(ActionEvent actionEvent) {
+            handleMoveDown();
+        }
+
+    };
+
+    private Action importAction = new AbstractAction("Import from file", Icons.getIcon("project.open.gif")){
+        public void actionPerformed(ActionEvent actionEvent) {
+            handleImport();
+        }
+
+    };
+
+
+    private Action exportAction = new AbstractAction("Export to file", Icons.getIcon("project.save.gif")){
+        public void actionPerformed(ActionEvent actionEvent) {
+            handleExport();
+        }
+
+    };
+
+    public void applyChanges() {
+        if (dirty){
+            AnnotationTemplatePrefs.getInstance().putValues(getValuesFromTable());
+            dirty = false;
+        }
+    }
+
+
+    private List<String> getValuesFromTable() {
+        List<String> values = new ArrayList<String>();
+        for (int i=0; i<model.getRowCount(); i++){
+            String str = "";
+            for (int j=0; j<model.getColumnCount(); j++){
+                if (j>0){
+                    str+=",";
+                }
+                final Object v = model.getValueAt(i, j);
+                if (v != null){
+                    str+= v;
+                }
+            }
+            values.add(str);
+        }
+        return values;
+    }
+
+
+    public void initialise() throws Exception {
+
+        setLayout(new BorderLayout());
+
+        toolbar = new JToolBar();
+        toolbar.setFloatable(false);
+        addToolbarAction(addAction);
+        addToolbarAction(removeAction);
+        toolbar.addSeparator(new Dimension(6, 6));
+        addToolbarAction(upAction);
+        addToolbarAction(downAction);
+        toolbar.addSeparator(new Dimension(6, 6));
+        addToolbarAction(importAction);
+        addToolbarAction(exportAction);
+
+        add(toolbar, BorderLayout.NORTH);
+
+        List<String> rows = AnnotationTemplatePrefs.getInstance().getValues();
+        model = new MyTableModel(rows);
+        table = new JTable(model);
+        table.setShowVerticalLines(false);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+        table.getColumnModel().getColumn(0).setWidth(200);
+        final JScrollPane scroller = new JScrollPane(table);
+//        scroller.setPreferredSize(new Dimension(400, 300));
+        add(scroller, BorderLayout.CENTER);
+    }
+
+
+    private void addToolbarAction(Action action) {
+        JButton button = new JButton(action);
+        button.setToolTipText((String)action.getValue(Action.NAME));
+        button.setText(null);
+        button.setBorder(new EmptyBorder(4, 4, 4, 4));
+        toolbar.add(button);
+    }
+
+
+    public void dispose() throws Exception {
+        // do nothing
+    }
+
+    private void handleAddAnnotation() {
+        AnnotationURIList list = new AnnotationURIList(getOWLEditorKit());
+        list.rebuildAnnotationURIList();
+        final JScrollPane scroller = new JScrollPane(list);
+        scroller.setPreferredSize(new Dimension(400, 300));
+        if (JOptionPane.showConfirmDialog(AnnotationTemplatePrefsPanel.this, scroller, "Pick an annotation",
+                                          JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE) == JOptionPane.OK_OPTION){
+            Object[] rowData = new Object[]{list.getSelectedURI(), null};
+            model.addRow(rowData);
+            table.getSelectionModel().setSelectionInterval(model.getRowCount()-1, model.getRowCount()-1);
+            dirty = true;
+        }
+    }
+
+
+    private void handleRemoveAnnotation() {
+        final int row = table.getSelectedRow();
+        if (row != -1){
+            model.removeRow(row);
+            if (row < model.getRowCount()){
+                table.getSelectionModel().setSelectionInterval(row, row);
+            }
+            else if (row-1 > 0){
+                table.getSelectionModel().setSelectionInterval(row-1, row-1);
+            }
+            dirty = true;
+        }
+    }
+
+    private void handleMoveUp() {
+        final int row = table.getSelectedRow();
+        if (row > 0){
+            model.moveRow(row, row, row-1);
+            table.getSelectionModel().setSelectionInterval(row-1, row-1);
+            dirty = true;
+        }
+    }
+
+    private void handleMoveDown() {
+        final int row = table.getSelectedRow();
+        if (row < model.getRowCount()-1){
+            model.moveRow(row, row, row+1);
+            table.getSelectionModel().setSelectionInterval(row+1, row+1);
+            dirty = true;
+        }
+    }
+
+
+    private void handleImport() {
+        JFrame f = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, getParent());
+        if (f == null) {
+            f = new JFrame();
+        }
+
+        File importFile = UIUtil.openFile(f, "Import a template file for your annotations", Collections.EMPTY_SET);
+        try {
+            FileInputStream inStream = new FileInputStream(importFile);
+            List<String> rows = AnnotationTemplatePrefs.parseStream(inStream);
+            model = new MyTableModel(rows);
+            table.setModel(model);
+            dirty = true;
+            inStream.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleExport() {
+        JFrame f = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, getParent());
+        if (f == null) {
+            f = new JFrame();
+        }
+
+        File importFile = UIUtil.saveFile(f, "Export a template file for your annotations", Collections.EMPTY_SET);
+        try {
+            PrintStream outStream = new PrintStream(new FileOutputStream(importFile));
+            for (String row : getValuesFromTable()){
+                outStream.println(row);
+            }
+            outStream.flush();
+            outStream.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private class MyTableModel extends DefaultTableModel {
+
+        private MyTableModel(List<String> rows) {
+            addColumn("uri");
+            addColumn("params");
+
+            for (String row : rows){
+                addRow(row.split(","));
+            }
+        }
+    }
+}
