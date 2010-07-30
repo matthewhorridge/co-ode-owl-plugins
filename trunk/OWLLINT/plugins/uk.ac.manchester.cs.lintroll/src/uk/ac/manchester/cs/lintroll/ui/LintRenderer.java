5f4b60626b7424dd844d42735be89fdb40dd19be
therealluigi <therealluigi@1761ad68-4734-0410-b8c8-2b8105e0dd08>
2010-07-30 08:09:18 +0000

    20100730 SVN Cleanup

--- trunk/OWLLINT/plugins/uk.ac.manchester.cs.lintroll/src/uk/ac/manchester/cs/lintroll/ui/LintRenderer.java
+++ trunk/OWLLINT/plugins/uk.ac.manchester.cs.lintroll/src/uk/ac/manchester/cs/lintroll/ui/LintRenderer.java
@@ -33,14 +33,15 @@ import org.semanticweb.owl.model.OWLObject;
  * @author Luigi Iannone
  * 
  */
-public class LintRenderer extends OWLCellRenderer implements TreeCellRenderer, ListCellRenderer {
+public class LintRenderer extends OWLCellRenderer implements TreeCellRenderer,
+		ListCellRenderer {
 	private OWLCellRenderer owlCellRenderer;
 	private DefaultTreeCellRenderer defaultTreeCellRenderer = new DefaultTreeCellRenderer();
 	private DefaultListCellRenderer defaultListCellRenderer = new DefaultListCellRenderer();
-	public static final Color SELECTION_BACKGROUND = UIManager.getDefaults().getColor(
-			"List.selectionBackground");
-	public static final Color SELECTION_FOREGROUND = UIManager.getDefaults().getColor(
-			"List.selectionForeground");
+	public static final Color SELECTION_BACKGROUND = UIManager.getDefaults()
+			.getColor("List.selectionBackground");
+	public static final Color SELECTION_FOREGROUND = UIManager.getDefaults()
+			.getColor("List.selectionForeground");
 
 	public LintRenderer(OWLEditorKit owlEditorKit) {
 		super(owlEditorKit);
@@ -50,78 +51,70 @@ public class LintRenderer extends OWLCellRenderer implements TreeCellRenderer, L
 	}
 
 	@Override
-	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
-			boolean expanded, boolean leaf, int row, boolean hasFocus) {
+	public Component getTreeCellRendererComponent(JTree tree, Object value,
+			boolean selected, boolean expanded, boolean leaf, int row,
+			boolean hasFocus) {
 		Component toReturn;
 		DefaultMutableTreeNode valueNode = (DefaultMutableTreeNode) value;
 		if (valueNode.isRoot()) {
-			URL url = this.getClass().getClassLoader().getResource("lintroll.jpg");
+			URL url = this.getClass().getClassLoader().getResource(
+					"lintroll.jpg");
 			ImageIcon newIcon = new ImageIcon(url);
 			if (valueNode.isLeaf()) {
 				this.defaultTreeCellRenderer.setLeafIcon(newIcon);
-				toReturn = this.defaultTreeCellRenderer.getTreeCellRendererComponent(
-						tree,
-						value,
-						selected,
-						expanded,
-						leaf,
-						row,
-						hasFocus);
-				this.defaultTreeCellRenderer.setLeafIcon(this.defaultTreeCellRenderer.getDefaultLeafIcon());
+				toReturn = this.defaultTreeCellRenderer
+						.getTreeCellRendererComponent(tree, value, selected,
+								expanded, leaf, row, hasFocus);
+				this.defaultTreeCellRenderer
+						.setLeafIcon(this.defaultTreeCellRenderer
+								.getDefaultLeafIcon());
 			} else {
 				this.defaultTreeCellRenderer.setOpenIcon(newIcon);
 				this.defaultTreeCellRenderer.setClosedIcon(newIcon);
-				toReturn = this.defaultTreeCellRenderer.getTreeCellRendererComponent(
-						tree,
-						"",
-						selected,
-						expanded,
-						leaf,
-						row,
-						hasFocus);
-				this.defaultTreeCellRenderer.setOpenIcon(this.defaultTreeCellRenderer.getDefaultOpenIcon());
-				this.defaultTreeCellRenderer.setClosedIcon(this.defaultTreeCellRenderer.getDefaultClosedIcon());
+				toReturn = this.defaultTreeCellRenderer
+						.getTreeCellRendererComponent(tree, "", selected,
+								expanded, leaf, row, hasFocus);
+				this.defaultTreeCellRenderer
+						.setOpenIcon(this.defaultTreeCellRenderer
+								.getDefaultOpenIcon());
+				this.defaultTreeCellRenderer
+						.setClosedIcon(this.defaultTreeCellRenderer
+								.getDefaultClosedIcon());
 			}
 		} else {
 			Object nodeUserObject = valueNode.getUserObject();
 			if (nodeUserObject instanceof OWLObject) {
 				toReturn = this.owlCellRenderer.getTreeCellRendererComponent(
-						tree,
-						nodeUserObject,
-						selected,
-						expanded,
-						leaf,
-						row,
+						tree, nodeUserObject, selected, expanded, leaf, row,
 						hasFocus);
-			} else if (nodeUserObject instanceof LintReport) {
+			} else if (nodeUserObject instanceof LintReport<?>) {
 				toReturn = new JPanel(new BorderLayout());
 				JTextPane textPane = new JTextPane();
 				textPane.setOpaque(false);
 				((JPanel) toReturn).add(textPane, BorderLayout.CENTER);
 				LintReport<?> report = (LintReport<?>) nodeUserObject;
 				Lint<?> lint = report.getLint();
-				if (lint instanceof ActingLint) {
-					URL url = this.getClass().getClassLoader().getResource("hammer.jpg");
+				if (lint instanceof ActingLint<?>) {
+					URL url = this.getClass().getClassLoader().getResource(
+							"hammer.jpg");
 					ImageIcon icon = new ImageIcon(url);
-					((JPanel) toReturn).add(new JLabel(icon), BorderLayout.EAST);
+					((JPanel) toReturn)
+							.add(new JLabel(icon), BorderLayout.EAST);
 				}
-				textPane.setText(lint.getName() + "{" + report.getAffectedOntologies().size() + "}");
+				textPane.setText(lint.getName() + "{"
+						+ report.getAffectedOntologies().size() + "}");
 				this.render(tree, toReturn, selected);
 			} else {
-				toReturn = this.defaultTreeCellRenderer.getTreeCellRendererComponent(
-						tree,
-						value,
-						selected,
-						expanded,
-						leaf,
-						row,
-						hasFocus);
+				toReturn = this.defaultTreeCellRenderer
+						.getTreeCellRendererComponent(tree, value, selected,
+								expanded, leaf, row, hasFocus);
 			}
 		}
 		return toReturn;
 	}
 
-	private void render(Component renderingComponent, Component toReturn, boolean selected) {
+	private void render(Component renderingComponent, Component toReturn,
+			boolean selected) {
 		if (selected) {
 			toReturn.setBackground(SELECTION_BACKGROUND);
 			toReturn.setForeground(SELECTION_FOREGROUND);
@@ -133,22 +126,20 @@ public class LintRenderer extends OWLCellRenderer implements TreeCellRenderer, L
 	}
 
 	@Override
-	public Component getListCellRendererComponent(JList list, Object value, int index,
-			boolean isSelected, boolean cellHasFocus) {
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
 		Component toReturn;
-		if (value instanceof Lint) {
+		if (value instanceof Lint<?>) {
 			toReturn = new JTextPane();
 			((JTextPane) toReturn).setText(((Lint<?>) value).getName());
-		} else if (value instanceof LintPattern) {
+		} else if (value instanceof LintPattern<?>) {
 			toReturn = new JTextPane();
-			((JTextPane) toReturn).setText(((LintPattern<?>) value).getClass().getSimpleName());
+			((JTextPane) toReturn).setText(((LintPattern<?>) value).getClass()
+					.getSimpleName());
 		} else {
-			toReturn = this.defaultListCellRenderer.getListCellRendererComponent(
-					list,
-					value,
-					index,
-					isSelected,
-					cellHasFocus);
+			toReturn = this.defaultListCellRenderer
+					.getListCellRendererComponent(list, value, index,
+							isSelected, cellHasFocus);
 		}
 		this.render(list, toReturn, isSelected);
 		return toReturn;
